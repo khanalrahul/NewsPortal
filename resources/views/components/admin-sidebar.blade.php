8381be55430f8f383e36b0556729770da25fedba
Rahul Khanal <khanalrahul79@gmail.com>
2024-12-17 19:37:41 +0545

    Designed Admin FrontEnd view using OTIKA

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,36 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html"> <i data-feather="book" class="header-logo">
+
+        </i>
+            <span class="logo-name">NewsPortal</span>
+        </a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+            <a href="{{route('dashboard')}}" class="nav-link"><i
+                    data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                    data-feather="settings"></i><span>Settings</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="widget-chart.html">Company Details</a></li>
+                <li><a class="nav-link" href="widget-data.html">Category Details</a></li>
+            </ul>
+        </li>
+
+        <li class="menu-header">Manage</li>
+        <li class="dropdown active">
+            <a href="index.html" class="nav-link"><i
+                    data-feather="clipboard"></i><span>Post</span></a>
+        </li>
+        <li class="dropdown active">
+            <a href="index.html" class="nav-link"><i
+                    data-feather="film"></i><span>Advertise</span></a>
+        </li>
+        
+    </ul>
+</aside>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,91 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>NewsPortal - Admin Dashboard</title>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="assets/css/app.min.css">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+</head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<body>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar sticky">
+                <div class="form-inline mr-auto">
+                    <ul class="navbar-nav mr-3">
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg
+									collapse-btn">
+                                <i data-feather="align-justify"></i></a></li>
+                        <li><a href="#" class="nav-link nav-link-lg fullscreen-btn">
+                                <i data-feather="maximize"></i>
+                            </a></li>
+                    </ul>
+                </div>
+                <ul class="navbar-nav navbar-right">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown"
+                            class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <i data-feather="settings"></i>
+                            <span class="d-sm-none d-lg-inline-block"></span></a>
+                        <div class="dropdown-menu dropdown-menu-right pullDown">
+                            <div class="dropdown-title">Hello {{ Auth::user()->name }}!</div>
+                            <a href="{{ route('profile.edit') }}" class="dropdown-item has-icon"> <i
+                                    class="far
+										fa-user"></i>
+                                Profile
+                            </a>
+                            <div class="dropdown-divider"></div>
+                          
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+                                <x-responsive-nav-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                    this.closest('form').submit();" class="dropdown-item has-icon text-danger">
+                                    <i class="fas fa-sign-out-alt"></i>
+                                    {{ __('Log Out') }}
+                                </x-responsive-nav-link>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div class="main-sidebar sidebar-style-2">
+                <x-admin-sidebar />
+            </div>
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section">
+                    {{ $slot }}
+                </section>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            </div>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    <a href="#">Newsportal</a></a>
+                </div>
+                <div class="footer-right">
+                </div>
+            </footer>
         </div>
-    </body>
+    </div>
+    <!-- General JS Scripts -->
+    <script src="assets/js/app.min.js"></script>
+    <!-- JS Libraies -->
+    <script src="assets/bundles/apexcharts/apexcharts.min.js"></script>
+    <!-- Page Specific JS File -->
+    <script src="assets/js/page/index.js"></script>
+    <!-- Template JS File -->
+    <script src="assets/js/scripts.js"></script>
+    <!-- Custom JS File -->
+    <script src="assets/js/custom.js"></script>
+</body>
+
 </html>
